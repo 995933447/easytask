cc7b020357face1a82a8e55091863cffc2124f07
yangzhouhong <yangzhouhong@quanliang.com>
2022-12-06 02:06:12 +0800

    ok

--- internal/util/errs/err.go
+++ internal/util/errs/err.go
@@ -2,27 +2,9 @@ package errs
 
 import (
 	"errors"
-	"sync"
 )
 
 var (
 	ErrCurrentNodeNoMaster = errors.New("current node is not master")
 	ErrServerStarted = errors.New("server started")
 )
-
-type AtomicErr struct {
-	mu sync.RWMutex
-	err error
-}
-
-func (e *AtomicErr) SetErr(err error) {
-	e.mu.Lock()
-	defer e.mu.Unlock()
-	e.err = err
-}
-
-func (e *AtomicErr) GetErr() error {
-	e.mu.RLock()
-	defer e.mu.Unlock()
-	return e.err
-}
