849bcc9d5ccb624dc89b6acf16687fd221eaacb2
yangzhouhong <yangzhouhong@quanliang.com>
2022-12-06 16:15:41 +0800

    ok

--- pkg/contx/ctx.go
+++ pkg/contx/ctx.go
@@ -6,7 +6,7 @@ import (
 )
 
 func New(moduleName string, ctx context.Context) context.Context {
-	return simpletracectx.New(moduleName, context.TODO(), "", "")
+	return simpletracectx.New(moduleName, ctx, "", "")
 }
 
 func ChildOf(ctx context.Context) context.Context {
