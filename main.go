06f24e0a7e3bed53220fa8c3b19fdabeff22e6c9
yangzhouhong <yangzhouhong@quanliang.com>
2022-12-23 17:05:17 +0800

    ok

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/995933447/autoelect"
 	electfactory "github.com/995933447/autoelect/factory"
 	"github.com/995933447/confloader"
@@ -21,8 +20,6 @@ import (
 	"github.com/etcd-io/etcd/client"
 	"os"
 	"os/signal"
-	"runtime/debug"
-	"strings"
 	"syscall"
 	"time"
 )
